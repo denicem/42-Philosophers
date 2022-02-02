c20bc4f1bc8445914d5d2e0c354c11a2570b318c
Denice Montemayor <dmontema@1-D-15.42heilbronn.de>
2022-02-02 19:48:51 +0100

    pthread_join with a return var.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:16:14 by dmontema          #+#    #+#             */
-/*   Updated: 2022/02/02 11:37:10 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/02/02 13:35:50 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <time.h>
 
 int count = 0;
 pthread_mutex_t mutex;
@@ -27,29 +28,52 @@ void *routine()
 	return (NULL);
 }
 
+void *roll_dice(void *arg)
+{
+	(void) arg;
+	int *res = malloc(sizeof(int));
+	int val = (rand() % 6) + 1;
+
+	*res = val;
+	return ((void *) res);
+}
+
 int	main(int argc, char *argv[])
 {
 	(void) argc;
 	(void) argv;
-	int i = 0;
-	int loop = 4;
+	// {
+	// 	int i = 0;
+	// 	int loop = 8;
 
-	pthread_t	t1[loop];
+	// 	pthread_t	thds[loop];
 
-	pthread_mutex_init(&mutex, NULL);
-	while (i < loop)
+	// 	pthread_mutex_init(&mutex, NULL);
+	// 	while (i < loop)
+	// 	{
+	// 		pthread_create(thds + i, NULL, &routine, NULL);
+	// 		printf("Thread %d started.\n", i++);
+	// 	}
+	// 	i = 0;
+	// 	while (i < loop)
+	// 	{
+	// 	pthread_join(thds[i], NULL);
+	// 		printf("Thread %d finished execution. - %d\n", i++, count);
+	// 	}
+	// 	pthread_mutex_destroy(&mutex);
+	// 	printf("%d\n", count);
+	// }
+
 	{
-		pthread_create(t1 + i, NULL, &routine, NULL);
-		printf("Thread %d started.\n", i++);
+		int *res;
+		srand(time(NULL));
+		pthread_t	th;
+		pthread_create(&th, NULL, &roll_dice, NULL);
+		pthread_join(th, (void **) &res);
+		printf("You diced %d\n", *res);
+		free(res);
 	}
-	i = 0;
-	while (i < loop)
-	{
-	pthread_join(t1[i], NULL);
-		printf("Thread %d finished execution.\n", i++);
-	}
-	pthread_mutex_destroy(&mutex);
-	printf("%d\n", count);
+
 
 	return (0);
 }
