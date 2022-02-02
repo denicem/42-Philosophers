cc2c66e9d64c94f36044ddfc0eb98a2f37d1b4dd
Denice Montemayor <dmontema@1-D-15.42heilbronn.de>
2022-02-02 12:23:47 +0100

    experimented with threads and mutex. add gitignore.

--- main.c
+++ main.c
@@ -6,16 +6,50 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:16:14 by dmontema          #+#    #+#             */
-/*   Updated: 2022/02/01 16:17:20 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/02/02 11:37:10 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <pthread.h>
+
+int count = 0;
+pthread_mutex_t mutex;
+
+void *routine()
+{
+	pthread_mutex_lock(&mutex);
+	for (int i = 0; i < 10000000; i++)
+		count++;
+	pthread_mutex_unlock(&mutex);
+	return (NULL);
+}
 
 int	main(int argc, char *argv[])
 {
 	(void) argc;
 	(void) argv;
+	int i = 0;
+	int loop = 4;
+
+	pthread_t	t1[loop];
+
+	pthread_mutex_init(&mutex, NULL);
+	while (i < loop)
+	{
+		pthread_create(t1 + i, NULL, &routine, NULL);
+		printf("Thread %d started.\n", i++);
+	}
+	i = 0;
+	while (i < loop)
+	{
+	pthread_join(t1[i], NULL);
+		printf("Thread %d finished execution.\n", i++);
+	}
+	pthread_mutex_destroy(&mutex);
+	printf("%d\n", count);
 
 	return (0);
 }
