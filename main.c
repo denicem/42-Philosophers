7eaf420e546b22a75b9603d4fd1776579ad79e0c
Denice Montemayor <dmontema@1-D-13.42heilbronn.de>
2022-02-14 17:03:52 +0100

    creating/initializing Philosophers/Threads and Mutexes now possible.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:16:14 by dmontema          #+#    #+#             */
-/*   Updated: 2022/02/13 20:37:56 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/02/14 17:01:38 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,33 +43,12 @@
 // 	return ((void *) res);
 // }
 
-t_info	*info()
+void *print_create()
 {
-	static t_info	info;
-
-	return (&info);
+	printf("Created.\n");
+	return (NULL);
 }
 
-void init_info(char **argv)
-{
-	(void) argv;
-	info()->count_philo = atoi(argv[1]);
-	info()->time_to_die = atoi(argv[2]);
-	info()->time_to_eat = atoi(argv[3]);
-	info()->time_to_sleep = atoi(argv[4]);
-	if (argv[5])
-		info()->count_meal = atoi(argv[5]);
-}
-
-void	print_info()
-{
-	printf("INFO:\n");
-	printf("Philosophers:\t%d\n", info()->count_philo);
-	printf("Time to die:\t%d\n", info()->time_to_die);
-	printf("Time to eat:\t%d\n", info()->time_to_eat);
-	printf("Time to sleep:\t%d\n", info()->time_to_sleep);
-	printf("Meal goals:\t%d\n", info()->count_meal);
-}
 
 int	main(int argc, char *argv[])
 {
@@ -108,15 +87,119 @@ int	main(int argc, char *argv[])
 	// }
 
 	{
-		t_info	*information;
-		
-		information = info();
 		if (argc == 5 || argc == 6)
 		{
 			init_info(argv);
 			print_info();
 		}
 	}
-
 	return (0);
 }
+
+t_info	*info()
+{
+	static t_info	info;
+
+	return (&info);
+}
+
+
+void init_info(char **argv)
+{
+	(void) argv;
+	info()->count_philo = atoi(argv[1]);
+	info()->time_to_die = atoi(argv[2]);
+	info()->time_to_eat = atoi(argv[3]);
+	info()->time_to_sleep = atoi(argv[4]);
+	if (argv[5])
+		info()->count_meal = atoi(argv[5]);
+	info()->philos = malloc(sizeof(t_philo **) * info()->count_philo);
+	init_philos();
+}
+
+void	print_info()
+{
+	printf("INFO:\n");
+	printf("Philosophers:\t%d\n", info()->count_philo);
+	printf("Time to die:\t%d\n", info()->time_to_die);
+	printf("Time to eat:\t%d\n", info()->time_to_eat);
+	printf("Time to sleep:\t%d\n", info()->time_to_sleep);
+	printf("Meal goals:\t%d\n", info()->count_meal);
+}
+
+////////////////////////////////////////////////////////////////////////
+
+
+void	init_philos()
+{
+	int i;
+	int count_philo;
+
+	i = 0;
+	count_philo = info()->count_philo;
+	while (i < count_philo)
+	{
+		info()->philos[i] = malloc(sizeof(t_philo *));
+		info()->philos[i]->id = i;
+		if (pthread_create(&info()->philos[i]->p_id, NULL, &print_create, NULL));
+			printf("Philosopher won't join. :(");
+		if (pthread_mutex_init(&info()->philos[i]->fork, NULL))
+			printf("Philosopher didn't get a fork. :(");
+		i++;
+	}
+}
+
+//////////////////////// DUMP
+
+// t_philo	*new_philo(int th_id)
+// {
+// 	t_philo *new;
+
+// 	new = (t_philo *) malloc(sizeof(t_philo *));
+// 	if (!new)
+// 		return (NULL);
+// 	new->id = th_id;
+// 	// pthread_create(&(new->philo), NULL, &print_create, NULL);
+// 	// pthread_mutex_init(&(new->fork), NULL);
+// 	new->next = new;
+// 	return (new);
+// }
+
+// int	insert_philo(t_philo **head, t_philo *new)
+// {
+// 	t_philo	*tmp;
+// 	int h_id;
+
+// 	if (!new)
+// 		return (0);
+// 	if (!(*head))
+// 	{
+// 		*head = new;
+// 		(*head)->next = *head;
+// 		return (1);
+// 	}
+// 	tmp = *head;
+// 	h_id = tmp->id;
+// 	while (tmp->next->id != (*head)->id)
+// 		tmp = tmp->next;
+// 	new->next = *head;
+// 	tmp->next = new;
+// 	return (1);
+// }
+
+// void	print_philos(t_philo *head)
+// {
+// 	t_philo	*tmp;
+
+// 	if (!head)
+// 		return ;
+// 	tmp = head;
+// 	printf("Philosopher: %d\n", tmp->id);
+// 	tmp = tmp->next;
+// 	while (tmp->id != head->id)
+// 	{
+// 	// printf("OK\n");
+// 		printf("Philosopher: %d\n", tmp->id);
+// 		tmp = tmp->next;
+// 	}
+// }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 18:14:59 by dmontema          #+#    #+#             */
-/*   Updated: 2022/02/13 20:32:16 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/02/14 17:01:43 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,27 @@
 typedef struct s_philo
 {
 	int				id;
-	pthread_t		philo;
+	pthread_t		p_id;
 	pthread_mutex_t	fork;
-	struct s_philo	*next;
 }	t_philo;
+
+void	init_philos();
 
 typedef struct s_info
 {
-	int	count_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	count_meal;
+	int		count_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		count_meal;
+	t_philo	**philos;
 }	t_info;
 
+t_info	*info();
+void	init_info(char **argv);
+void	print_info();
+
+// DUMP
+// t_philo	*new_philo(int th_id);
+// int		insert_philo(t_philo **head, t_philo *new);
+// void	print_philos(t_philo *head);
